c24570bb01b0d1e805c7c12f0e9f4d7a2f491cf4
Adam Hardy <adh167@gmail.com>
2021-03-18 18:05:44 +0000

    added test file

---
+++ test.py
@@ -0,0 +1,72 @@
+import os
+from time import sleep
+from selenium import webdriver
+from multi_webbing import multi_webbing as mw
+
+def job_function_selenium(job):
+    job.get_url()
+
+    print(job.driver.current_url)
+
+def job_function_requests(job):
+    job.get_url()
+
+    print(job.request.url)
+
+os.environ["DISPLAY"] = ":0"
+
+test = {}
+
+print("Testing selenium:")
+try:
+    num_threads = 2
+    my_threads = mw.MultiWebbing(num_threads, "selenium")
+    my_threads.start()
+
+
+    for i in range(4):
+        my_threads.job_queue.put(mw.Job(job_function_selenium, f"https://www.google.com/search?q={i}", "string", ))
+
+
+    while my_threads.job_queue.qsize() > 0:
+        sleep(5)
+
+    my_threads.finish()
+
+except:
+    test["selenium"] = False
+
+else:
+    test["selenium"] = True
+
+print("Selenium test complete, testing requests:")
+
+try:
+    num_threads = 2
+    my_threads = mw.MultiWebbing(num_threads)
+    my_threads.start()
+
+
+    for i in range(4):
+        my_threads.job_queue.put(mw.Job(job_function_requests, f"https://www.google.com/search?q={i}", "string", ))
+
+
+    while my_threads.job_queue.qsize() > 0:
+        sleep(5)
+
+    my_threads.finish()
+
+except:
+    test["requests"] = False
+
+else:
+    test["requests"] = True
+
+print("Tests Complete")
+
+for key,val in test.items():
+    if val:
+        print (f"{key} test successful.")
+    else:
+        print (f"{key} test failed.")
+
