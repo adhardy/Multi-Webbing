0f7a448be37aad7c182a0d6041534ee3693f9a43
Adam Hardy <adh167@gmail.com>
2021-03-18 18:04:34 +0000

    incremented verion number

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='multi-webbing',
-    version='0.1.0',    
-    description='A multi-threaded libary for web scraping in python, built upon the python threading and requests modules.',
+    version='0.2.0',    
+    description='A multi-threaded libary for web scraping in python, built upon the python threading. Supports selenium and requests.',
     url='https://github.com/adhardy/Multi-Webbing',
     author='Adam Hardy',
     author_email='adamdhardy@icloud.com',
